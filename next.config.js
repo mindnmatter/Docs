a3e247dbd8b56c1e9a1e553291a908d1bd4a78b2
Aman Singh <besoeasy@users.noreply.github.com>
2021-01-04 23:48:18 +0000

    new

--- next.config.js
+++
@@ -1,8 +0,0 @@
-const withNextra = require('nextra')('nextra-theme-docs', './theme.config.js')
-module.exports = withNextra({
-  distDir: 'build',
-  i18n: {
-    locales: ['en'],
-    defaultLocale: 'en',
-  },
-})
--- pages/index.en.md
+++
@@ -1,6 +0,0 @@
-# THIRM
-
-Hello
-
-
-
