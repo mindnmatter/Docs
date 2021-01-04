21ac488c61b7752961e7153677d37b2de1d7ad61
Aman Singh <besoeasy@users.noreply.github.com>
2021-01-04 02:40:44 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 const withNextra = require('nextra')('nextra-theme-docs', './theme.config.js')
 module.exports = withNextra({
+  distDir: 'build',
   i18n: {
     locales: ['en'],
     defaultLocale: 'en',
