94375547cf033f80c13332b4eb3a509abd6012ff
besoeasy <besoeasy@users.noreply.github.com>
2021-01-05 01:57:43 +0000

    Deploying to gh-pages from  @ d9b59e443facf6bb541d54bff1ac3db150bf52dc 🚀

--- _nuxt/static/1609811470/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Freleases","\u002F","\u002FVM\u002FIntroduction","\u002FTTOKENS\u002FList","\u002FTTOKENS\u002FIntroduction","\u002FTOKEN\u002FTokenomics","\u002FTOKEN\u002FIntroduction"]})
